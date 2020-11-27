be4b0d3036a88ccc262807dbf5321fce68c4bf6c
Gab Fitzgerald <gab@Gabs-MacBook-Pro.local>
2020-11-27 11:39:11 +1000

    data type wrangling

--- src/flux/query_result.cr
+++ src/flux/query_result.cr
@@ -23,7 +23,7 @@ module Flux::QueryResult
     tables = [] of Table(T)
 
     AnnotatedCSV.new(string_or_io, headers: true).each do |csv|
-      idx = csv["table"].to_i64
+      idx = csv["table"]
       table = tables[idx]?
 
       unless table
